10152fe64d1f900a8d24e0ed9450b2a5388288b6
R1dO <r1denouden@gmail.com>
2015-10-04 18:33:45 +0200

    Handle missing specialized config files.

--- tools/IDE/geany/openXcom.dev.sh
+++ tools/IDE/geany/openXcom.dev.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+dev_config_file=~/.config/openxcom/options.dev.cfg
+play_config_file=~/.config/openxcom/options.play.cfg
+config_file=~/.config/openxcom/options.cfg
+
+# Check if there are multiple config versions
+if [ ! -f $dev_config_file ]; then
+    cp $config_file $dev_config_file
+fi
+if [ ! -f $play_config_file ]; then
+    cp $config_file $play_config_file
+fi
+
 # Use dev version of config
-ln -sf options.cfg.dev ~/.config/openxcom/options.cfg
+ln -sf $dev_config_file $config_file
 
 # Start program.
 # Assumes geany basepath equals repository root, and the working dir of the
@@ -11,4 +23,4 @@ ln -sf options.cfg.dev ~/.config/openxcom/options.cfg
 echo "openXcom exit status = $?"
 
 # Return to play version of config
-ln -sf options.cfg.play ~/.config/openxcom/options.cfg
+ln -sf $play_config_file $config_file
