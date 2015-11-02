daa9bd6c640cc0a7f0518dc51be913d07a84c77f
R1dO <r1denouden@gmail.com>
2015-11-02 13:04:58 +0100

    Move ui debug logic to surface::draw()
    This makes it apply to more elements beside text (e.g. lists).

--- src/Engine/Surface.cpp
+++ src/Engine/Surface.cpp
@@ -43,6 +43,7 @@
 #ifdef __MORPHOS__
 #include <ppcinline/exec.h>
 #endif
+#include "../Engine/Options.h"
 
 namespace OpenXcom
 {
@@ -693,6 +694,22 @@ void Surface::draw()
 {
 	_redraw = false;
 	clear();
+
+	// Show text borders for debugging
+	if (Options::debugUi)
+	{
+		SDL_Rect r;
+		r.w = getWidth();
+		r.h = getHeight();
+		r.x = 0;
+		r.y = 0;
+		this->drawRect(&r, 5);
+		r.w-=2;
+		r.h-=2;
+		r.x++;
+		r.y++;
+		this->drawRect(&r, 0);
+	}
 }
 
 /**
