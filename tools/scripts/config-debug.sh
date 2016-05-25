befa2cefcd78a040e0903682d6bdece806e80ba1
R1dO <r1denouden@gmail.com>
2016-05-25 10:17:10 +0200

    clean up scripts
    Assume different repositories are used for different builds (own, master,
     extendend, etc ...) Which would help reducing build times at the cost of
    ~ 150MB per repository.

--- tools/scripts/config-debug.sh
+++ tools/scripts/config-debug.sh
@@ -1,11 +1,11 @@
 script_dir=$(dirname $(readlink -f $0))
 repo_dir=$(dirname $(dirname $script_dir))
 
-mkdir -p ${repo_dir}/build.master
-cd ${repo_dir}/build.master
+mkdir -p ${repo_dir}/build.debug
+cd ${repo_dir}/build.debug
 
 # Config + build
 cmake -G "Unix Makefiles" \
       -DCMAKE_INSTALL_PREFIX:PATH=/usr/local \
-      -DCMAKE_BUILD_TYPE:STRING=Release \
+      -DCMAKE_BUILD_TYPE:STRING=Debug \
       ..
--- tools/scripts/config-release.sh
+++ tools/scripts/config-release.sh
@@ -1,11 +1,11 @@
 script_dir=$(dirname $(readlink -f $0))
 repo_dir=$(dirname $(dirname $script_dir))
 
-mkdir -p ${repo_dir}/build.own.debug
-cd ${repo_dir}/build.own.debug
+mkdir -p ${repo_dir}/build
+cd ${repo_dir}/build
 
 # Config + build
 cmake -G "Unix Makefiles" \
       -DCMAKE_INSTALL_PREFIX:PATH=/usr/local \
-      -DCMAKE_BUILD_TYPE:STRING=Debug \
+      -DCMAKE_BUILD_TYPE:STRING=Release \
       ..
