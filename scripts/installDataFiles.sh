d0c549688ef11e18de240c8d91795618ceeed53d
R1dO <r1denouden@gmail.com>
2019-11-01 22:38:06 +0100

    Allow for verbose logging and improve library folders function.

--- scripts/installDataFiles.sh
+++ scripts/installDataFiles.sh
@@ -9,11 +9,14 @@
 # Global variables
 # ================
 DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+# Clutter the CLI with noise?
+VERBOSE=true
 
-# Steam variables
-# ---------------
+# Steam
+# -----
 STEAM_DATA_PATH="${DATA_HOME}/Steam"
-STEAM_LIBRARY_PATHS="${STEAM_DATA_PATH}/steamapps"
+STEAM_LIBRARY_PATHS="" #Set by 'parse_steam_libraryfolders_file()'.
+STEAM_LIBRARY_PATHS_DEFAULT="${STEAM_DATA_PATH}/steamapps"
 STEAM_ID_UFO="7760"
 STEAM_ID_TFTD="7650"
 
@@ -22,35 +25,36 @@ STEAM_ID_TFTD="7650"
 
 # Get the (user defined) library folders.
 #
-# Adds all folders to the global array: $STEAM_LIBRARY_PATHS
+# Sets the global array: $STEAM_LIBRARY_PATHS
 parse_steam_libraryfolders_file ()
 {
-	if [ ${#STEAM_LIBRARY_PATHS[@]} -gt 1 ]; then
-		printf "Line $BASH_LINENO: '${FUNCNAME[0]}' only needs to run once.\n"
-		return 1
-	fi
+	# Reset array to default value (in case we ran before).
+	unset STEAM_LIBRARY_PATHS
+	STEAM_LIBRARY_PATHS=${STEAM_LIBRARY_PATHS_DEFAULT}
 
+	# User defined folders are stored in this file.
 	vdf_file="${STEAM_LIBRARY_PATHS}/libraryfolders.vdf"
 
-	if [ -f "${vdf_file}" ]; then
+	if [ -f ${vdf_file} ]; then
 		: # Continue
 	else
 		printf "Line $BASH_LINENO: Could not find '${vdf_file}'.\n"
 		return 1
 	fi
 
-	# Only interested in the specific part containing the known locations:
+	# Only interested in the part with the known locations, for example:
 	# ``	"1"		"/opt/valve/SteamLibrary"``
 	#
 	# Supports up to 9 extra locations (to keep the regexp simple).
 	# Steam stores all relevant data under the sub-folder 'steamapps'.
 	STEAM_LIBRARY_PATHS+=($(cat ${vdf_file} | awk -F '\t' '{if($2 ~ /^"[1-9]"$/) {gsub(/"/,"",$4) ; print $4 "/steamapps"}}'))
 
-	# Reporting:
-	printf '\n%s\n' "Found the following ${#STEAM_LIBRARY_PATHS[@]} steam library paths:"
-	for ind in "${STEAM_LIBRARY_PATHS[@]}"; do
-		printf '  %s\n' "$ind"
-	done
+	if [ ${VERBOSE} = "true" ]; then
+		printf '\n%s\n' "Found the following ${#STEAM_LIBRARY_PATHS[@]} steam library paths:"
+		for ind in "${STEAM_LIBRARY_PATHS[@]}"; do
+			printf ' %s\n' "$ind"
+		done
+	fi
 }
 
 # Get the steam manifest file for selected game.
