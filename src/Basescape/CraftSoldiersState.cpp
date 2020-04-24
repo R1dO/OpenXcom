fa2f9c84069741910a6a610f693f80b9d4d0fa31
R1dO <r1denouden@gmail.com>
2020-04-24 22:39:55 +0200

    Increase visual separation between subtitle line and spreadsheet

--- src/Basescape/CraftSoldiersState.cpp
+++ src/Basescape/CraftSoldiersState.cpp
@@ -107,13 +107,13 @@ CraftSoldiersState::CraftSoldiersState(Base *base, size_t craft)
 	_window = new Window(this, 320, 200, 0, 0);
 	_btnOk = new TextButton(148, 16, 164, 176);
 	_txtTitle = new Text(300, 17, 16, 7);
-	_txtName = new Text(114, 9, 16, 32);
-	_txtRank = new Text(102, 9, 122, 32);
-	_txtCraft = new Text(84, 9, 224, 32);
+	_txtName = new Text(114, 9, 16, 34);
+	_txtRank = new Text(102, 9, 122, 34);
+	_txtCraft = new Text(84, 9, 224, 34);
 	_txtAvailable = new Text(110, 9, 16, 24);
 	_txtUsed = new Text(110, 9, 122, 24);
 	_cbxSortBy = new ComboBox(this, 148, 16, 8, 176, true);
-	_lstSoldiers = new TextList(288, 128, 8, 40);
+	_lstSoldiers = new TextList(288, 128, 8, 42);
 	_txtSoldiers = new Text(102, 9, 122, 24);
 	_txtVehicleUsage = new Text(84, 9, 224, 24);
 
