R1dO/OpenXcom

commit 3045a6a9ff6d5c88ffe9e87d6c60bccff9b7d56a

Fix broken string in Soldier Memorial screen

src/Engine/State.h
@@ -20,6 +20,7 @@
 #include <vector>
 #include <string>
 #include <SDL.h>
+#include "../Savegame/Soldier.h"
 
 namespace OpenXcom
 {
@@ -87,6 +88,8 @@ public:
 	const LocalizedText &tr(const std::string &id) const;
 	/// Get the localized text.
 	LocalizedText tr(const std::string &id, unsigned n) const;
+	/// Get the localized text.
+	const LocalizedText &tr(const std::string &id, SoldierGender gender) const;
 	/// redraw all the text-type surfaces.
 	void redrawText();
 	/// center all surfaces relative to the screen.

src/Engine/State.cpp
@@ -365,6 +365,18 @@ LocalizedText State::tr(const std::string &id, unsigned n) const
 }
 
 /**
+ * Get a modifiable copy of the localized text for dictionary key @a id.
+ * This function forwards the call to Language::getString(const std::string &, SoldierGender).
+ * @param id The dictionary key to search for.
+ * @param gender Current soldier gender.
+ * @return A reference to the localized text.
+ */
+const LocalizedText &State::tr(const std::string &id, SoldierGender gender) const
+{
+	return _game->getLanguage()->getString(id, gender);
+}
+
+/**
  * centers all the surfaces on the screen.
  */
 void State::centerAllSurfaces()

commit b6df3b476ae9a8013bad9170508d6632fee4ecb4

Fix compilation.
3045a6a introduced a circular reference problem for the previous commit.
e.g State depends on `Savegame/Soldier.h` which in turn depends on
`Mod/Unit.h`.
That commit in turn was needed to facilitate d008b5f

# Conflicts:
#	src/Engine/State.h
#	src/Savegame/Soldier.h

src/Engine/State.h
@@ -20,11 +20,12 @@
 #include <vector>
 #include <string>
 #include <SDL.h>
-#include "../Savegame/Soldier.h"
 #include "LocalizedText.h"
 
 namespace OpenXcom
 {
+
+enum SoldierGender { GENDER_MALE, GENDER_FEMALE };
 
 class Game;
 class Surface;

src/Savegame/Soldier.h
@@ -21,12 +21,12 @@
 #include <yaml-cpp/yaml.h>
 #include "../Mod/Unit.h"
 #include "../Mod/StatString.h"
+#include "../Engine/State.h"
 
 namespace OpenXcom
 {
 
 enum SoldierRank { RANK_ROOKIE, RANK_SQUADDIE, RANK_SERGEANT, RANK_CAPTAIN, RANK_COLONEL, RANK_COMMANDER};
-enum SoldierGender { GENDER_MALE, GENDER_FEMALE };
 enum SoldierLook { LOOK_BLONDE, LOOK_BROWNHAIR, LOOK_ORIENTAL, LOOK_AFRICAN };
 
 class Craft;