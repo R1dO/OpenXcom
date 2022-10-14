4f01b7480a0a82acaf71bb628f6a9dfa1e5e242a
R1dO <r1denouden@gmail.com>
2022-10-14 16:31:38 +0200

    Add filter option to show which item can be acquired.
    Helps in deciding what should to be safe to sell.

--- src/Basescape/SellState.h
+++ src/Basescape/SellState.h
@@ -72,6 +72,7 @@ private:
 		int transferSrc, transferDst;   ///< Amount currently on route **to** Src/Dst.
 		int allocatedSrc, allocatedDst; ///< Display only: Currently allocated items.
 		int protectedSrc, protectedDst; ///< Display only: Add this amount to display of ``qtySrc``.
+		bool canAcquire;                ///< Filter only: Whether or not we can buy/manufacture this item
 	};
 
 	Base *_base;
