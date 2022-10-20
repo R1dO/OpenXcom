c4ff1a449055bef548eab012926e94333e4b5431
R1dO <r1denouden@gmail.com>
2022-10-20 23:17:22 +0200

    Actually, showing fuel has it's merit.
    Tells player some plane is wasting their precious hoarded elerium stockpile.

--- src/Basescape/PurchaseState.cpp
+++ src/Basescape/PurchaseState.cpp
@@ -356,16 +356,16 @@ PurchaseState::PurchaseState(Base *base, CannotReequipState *parent) : _base(bas
 				int soldierArmor = _base->getItemClaimBySoldiers(rule, true, false)
 					- _base->getItemClaimBySoldiers(rule, true, true);
 
-				// Reserved amounts (future production).
+				// Reserved amounts (includes future production and craft fuel).
 				row.allocatedSrc = _base->getItemClaimByResearch(rule, true)
 					+ _base->getItemClaimByManufacture(rule, false, true)
-					+ _base->getItemClaimByCrafts(rule, false, true, true)
+					+ _base->getItemClaimByCrafts(rule, false, true, false)
 					+ soldierArmor;
 				// No 'on-base' display of the following categories:
 				// * Future production: Has not yet been taken from base stores.
 				row.protectedSrc = _base->getItemClaimByResearch(rule, true)
 					+ _base->getItemClaimByManufacture(rule, true, true)
-					+ _base->getItemClaimByCrafts(rule, true, true, true)
+					+ _base->getItemClaimByCrafts(rule, true, true, false)
 					+ soldierArmor;
 
 				row.qtySrc += row.transferSrc;
