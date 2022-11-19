ee3ecd0c9fa90897b73943e3813a6d625763ea44
R1dO <r1denouden@gmail.com>
2022-11-19 11:18:21 +0100

    Allow category filters for armors.
    QoL when player has to deal with missions imposing limits on allowed armors.
    
    Unsure about this:
    - Requires category definition on store item of all armors
    - Can lead to extra (weird) categories shown (if storeItem has multiple uses).

--- src/Basescape/SoldierArmorState.cpp
+++ src/Basescape/SoldierArmorState.cpp
@@ -30,6 +30,7 @@
 #include "../Interface/Text.h"
 #include "../Interface/TextEdit.h"
 #include "../Interface/TextList.h"
+#include "../Interface/ComboBox.h"
 #include "../Menu/ErrorMessageState.h"
 #include "../Mod/Armor.h"
 #include "../Mod/RuleInterface.h"
@@ -70,6 +71,7 @@ struct compareArmorName
 SoldierArmorState::SoldierArmorState(Base *base, size_t soldier, SoldierArmorOrigin origin) : _base(base), _soldier(soldier), _origin(origin)
 {
 	_screen = false;
+	_alternateScreen = Options::alternateBaseScreens;
 
 	// Create objects
 	_window = new Window(this, 192, 160, 64, 20, POPUP_BOTH);
@@ -80,6 +82,7 @@ SoldierArmorState::SoldierArmorState(Base *base, size_t soldier, SoldierArmorOri
 	_txtQuantity = new Text(70, 9, 190, 52);
 	_lstArmor = new TextList(160, 80, 73, 68);
 	_sortName = new ArrowButton(ARROW_NONE, 11, 8, 80, 52);
+	_cbxCategory = new ComboBox(this, 100, 16, 80, 52);
 
 	// Set palette
 	if (_origin == SA_BATTLESCAPE)
@@ -99,6 +102,7 @@ SoldierArmorState::SoldierArmorState(Base *base, size_t soldier, SoldierArmorOri
 	add(_txtQuantity, "text", "soldierArmor");
 	add(_lstArmor, "list", "soldierArmor");
 	add(_sortName, "text", "soldierArmor");
+	add(_cbxCategory, "text", "soldierArmor");
 
 	centerAllSurfaces();
 
@@ -125,6 +129,10 @@ SoldierArmorState::SoldierArmorState(Base *base, size_t soldier, SoldierArmorOri
 	_sortName->setX(_sortName->getX() + _txtType->getTextWidth() + 4);
 	_sortName->onMouseClick((ActionHandler)&SoldierArmorState::sortNameClick);
 
+	_cats.push_back("STR_TYPE");
+
+	// Don't depend on item listOrder, it does not exist for "STR_NONE" armors (storeItem is nullpointer).
+	int screenListOrder = 0;
 	const auto &armors = _game->getMod()->getArmorsForSoldiers();
 	for (auto* a : armors)
 	{
@@ -136,7 +144,7 @@ SoldierArmorState::SoldierArmorState(Base *base, size_t soldier, SoldierArmorOri
 		bool addSoldierArmor = (s->getArmor()->getStoreItem() == a->getStoreItem()); // True for the complete armor family
 		if (a->hasInfiniteSupply())
 		{
-			_armors.push_back(ArmorItem(a->getType(), tr(a->getType()), ""));
+			_armors.push_back(ArmorItem(a->getType(), tr(a->getType()), "", screenListOrder));
 		}
 		else if ((_base->getStorageItems()->getItem(a->getStoreItem()) + addSoldierArmor) > 0) // Uses integral promotion bool->int.
 		{
@@ -149,9 +157,42 @@ SoldierArmorState::SoldierArmorState(Base *base, size_t soldier, SoldierArmorOri
 			{
 				ss << "-";
 			}
-			_armors.push_back(ArmorItem(a->getType(), tr(a->getType()), ss.str()));
+			_armors.push_back(ArmorItem(a->getType(), tr(a->getType()), ss.str(), screenListOrder));
+		}
+		screenListOrder++;
+	}
+
+	// Armor categories
+	if (_game->getMod()->getDisplayCustomCategories() > 0)
+	{
+		// first find all relevant item categories
+		std::vector<std::string> tempCats;
+		for (std::vector<ArmorItem>::iterator i = _armors.begin(); i != _armors.end(); ++i)
+		{
+			const RuleItem *rule = _game->getMod()->getArmor((*i).type)->getStoreItem();
+			if (!rule || rule->getCategories().empty()) continue;
+
+			for (auto itemCat : rule->getCategories())
+			{
+				if (std::find(tempCats.begin(), tempCats.end(), itemCat) == tempCats.end())
+				{
+					tempCats.push_back(itemCat);
+				}
+			}
+		}
+		// Outport sorted by mod definition
+		const std::vector<std::string> &categories = _game->getMod()->getItemCategoriesList();
+		for (std::vector<std::string>::const_iterator k = categories.begin(); k != categories.end(); ++k)
+		{
+			if (std::find(tempCats.begin(), tempCats.end(), (*k)) != tempCats.end())
+			{
+				_cats.push_back((*k));
+			}
 		}
 	}
+
+	_cbxCategory->setOptions(_cats, true);
+	_cbxCategory->onChange((ActionHandler)&SoldierArmorState::cbxCategoryChange);
 
 	_btnQuickSearch->setText(""); // redraw
 	_btnQuickSearch->onEnter((ActionHandler)&SoldierArmorState::btnQuickSearchApply);
@@ -160,6 +201,7 @@ SoldierArmorState::SoldierArmorState(Base *base, size_t soldier, SoldierArmorOri
 	_btnCancel->onKeyboardRelease((ActionHandler)&SoldierArmorState::btnQuickSearchToggle, Options::keyToggleQuickSearch);
 
 	_armorOrder = ARMOR_SORT_NONE;
+	_previousOrder = ARMOR_SORT_NONE;
 	updateArrows();
 	updateList();
 
@@ -170,6 +212,16 @@ SoldierArmorState::SoldierArmorState(Base *base, size_t soldier, SoldierArmorOri
 	if (_origin == SA_BATTLESCAPE)
 	{
 		applyBattlescapeTheme("soldierArmor");
+	}
+
+	if (_alternateScreen)
+	{
+		_txtType->setVisible(false);
+		_sortName->setVisible(false);
+	}
+	else
+	{
+		_cbxCategory->setVisible(false);
 	}
 }
 
@@ -216,6 +268,7 @@ void SoldierArmorState::sortList()
 		std::sort(_armors.rbegin(), _armors.rend(), compareArmorName(true));
 		break;
 	default:
+		std::sort(_armors.begin(), _armors.end(), [](const ArmorItem a, const ArmorItem b) { return a.listOrder < b.listOrder; });
 		break;
 	}
 	updateList();
@@ -233,10 +286,22 @@ void SoldierArmorState::updateList()
 	_lstArmor->clearList();
 	_indices.clear();
 
+	size_t selCategory = _cbxCategory->getSelected();
+	const std::string selectedCategory = _cats[selCategory];
+	bool categoryFilterEnabled = (selectedCategory != "STR_TYPE");
+
 	int index = -1;
 	for (std::vector<ArmorItem>::const_iterator j = _armors.begin(); j != _armors.end(); ++j)
 	{
 		++index;
+
+		// filter
+		const RuleItem *rule = _game->getMod()->getArmor((*j).type)->getStoreItem();
+		// If armor is STR_NONE we have a nullpointer rule
+		if (categoryFilterEnabled && rule && !rule->belongsToCategory(selectedCategory))
+		{
+			continue;
+		}
 
 		// quick search
 		if (!searchString.empty())
@@ -353,6 +418,25 @@ void SoldierArmorState::sortNameClick(Action *)
 		_armorOrder = ARMOR_SORT_NAME_ASC;
 	}
 	updateArrows();
+	sortList();
+}
+
+/**
+* Updates the production list to match the category filter.
+*/
+void SoldierArmorState::cbxCategoryChange(Action *)
+{
+	_previousOrder = _armorOrder;
+
+	if (_game->isAltPressed())
+	{
+		_armorOrder = _game->isShiftPressed() ? ArmorSort::ARMOR_SORT_NAME_DESC : ArmorSort::ARMOR_SORT_NAME_ASC;
+	}
+	else
+	{
+		_armorOrder = ArmorSort::ARMOR_SORT_NONE;
+	}
+
 	sortList();
 }
 
